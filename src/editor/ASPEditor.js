e2e0ca5fc2692a1f7425e27deddc619e6b6e34b3
MattNot <matteoatnotaro@gmail.com>
2020-03-15 11:53:56 +0100

    Added Editor and Mode

---
+++ src/editor/ASPEditor.js
@@ -0,0 +1,33 @@
+import React from "react";
+import AceEditor from "react-ace";
+import CustomAspMode from "./Asp-Mode";
+import "brace/theme/dracula"
+
+class ASPEditor extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.aceEditor = React.createRef();
+    }
+
+    componentDidMount(): void {
+        this.aceEditor.current.editor.getSession().setMode(new CustomAspMode());
+        this.aceEditor.current.editor.setBehavioursEnabled(true);
+        this.aceEditor.current.editor.setWrapBehavioursEnabled(true);
+    }
+
+
+    render() {
+        return (
+            <AceEditor theme="dracula"
+                       mode="text"
+                       onChange={val => console.log(val)}
+                       name="Unico"
+                       editorProps={{$blockScrolling: true}}
+                       ref={this.aceEditor}
+            />
+        );
+    }
+}
+
+export default ASPEditor;
---
+++ src/editor/Asp-Mode.js
@@ -0,0 +1,91 @@
+import "brace/mode/java"
+
+export class AspHighLightsRules extends window.ace.acequire("ace/mode/text_highlight_rules").TextHighlightRules {
+    constructor() {
+        super();
+        this.$rules = {
+            "start": [
+                {
+                    token: "keyword", //Atoms with terms
+                    regex: "[a-z]\\w*\\(",
+                    next: "atomDef"
+                },
+                {
+                    token: "variable", //Atoms w/o terms
+                    regex: "[a-z]\\w*"
+                },
+                {
+                    token: "keyword",
+                    regex: ":-"
+                },
+                {
+                    token: "keyword",
+                    regex: "^:~"
+                },
+                {
+                    token: "keyword",
+                    regex: "\\."
+                },
+                {
+                    token: "keyword.operator",
+                    regex: "#(max|min|sum|count)\\{",
+                    next: "aggrCicle"
+                },
+                {
+                    token: "keyword.operator",
+                    regex: "}"
+                }
+            ],
+            "atomDef": [
+                {
+                    token: "variable",
+                    regex: "[A-Z]\\w*",
+                    next: "termList"
+                },
+                {
+                    token: "variable",
+                    regex: "\\w+",
+                    next: "termList"
+                }
+            ],
+            "termList": [
+                {
+                    token: "text",
+                    regex: ",",
+                    next: "atomDef"
+                },
+                {
+                    token: "keyword",
+                    regex: "\\)\\.*",
+                    next: "start"
+                }
+            ],
+            "aggrList": [
+                {
+                    token: "text",
+                    regex: ",",
+                    next: "aggrCicle"
+                },
+                {
+                    token: "text",
+                    regex: ":",
+                    next: "start"
+                }
+            ],
+            "aggrCicle": [
+                {
+                    token: "storage.type",
+                    regex: "[A-Z]\\w*",
+                    next: "aggrList"
+                }
+            ]
+        };
+    }
+}
+
+export default class CustomAspMode extends window.ace.acequire('ace/mode/java').Mode {
+    constructor() {
+        super();
+        this.HighlightRules = AspHighLightsRules;
+    }
+}
